4ba73f272f7123445ac58a497e57f1681ca3e02e
Tangi Beneat <tangi.beneat@gmail.com>
2025-10-01 14:37:52 +0200

    EDIT - modif structure of graph content

--- projects/ngx-forge-map/src/lib/emcapsulation/graph-forge.ts
+++ projects/ngx-forge-map/src/lib/emcapsulation/graph-forge.ts
@@ -22,10 +22,7 @@ export enum NodeShape {
   ICON = "icon",
 }
 
-/**
- * Empacsulation de la librairie Vis-Network [WIP]
- */
-export class ForgeGraph {
+export class GraphForge {
 
   private readonly _config: Options = {
     nodes: {
@@ -53,7 +50,8 @@ export class ForgeGraph {
 
   private readonly dataSet_nodes = new DataSet<NodeGraph>();
   private readonly dataSet_edges = new DataSet<EdgeGraph>();
-  private readonly test = new Map<NodeShape, Map<number, string>>()
+  private readonly ref_nodes = new Map<NodeShape, Map<number, string>>()
+  private readonly reverse_ref_node = new Map<string, { type: NodeShape, id: number }>();
 
   constructor(
     container: HTMLElement
@@ -65,47 +63,51 @@ export class ForgeGraph {
     );
   }
 
-  private _registerNewNode(node_type: NodeShape, node_ref_id: number, node_id: string) {
-    if (!this.test.has(node_type)) {
-      this.test.set(node_type, new Map());
+  private _registerNewNode(node_type: NodeShape, node_ref_id: number, node_id: string): boolean {
+    if (!this.ref_nodes.has(node_type)) {
+      this.ref_nodes.set(node_type, new Map());
     }
-    let e = this.test.get(node_type)!;
+    let e = this.ref_nodes.get(node_type)!;
 
+    if (e.has(node_ref_id)) return false;
     e.set(node_ref_id, node_id);
+    this.reverse_ref_node.set(node_id, { type: node_type, id: node_ref_id });
+    return true;
+  }
+
+  private _getReverseNodeRef(graph_node_id: string) {
+    return this.reverse_ref_node.get(graph_node_id);
   }
 
   private _getNodeRef(node_type: NodeShape, node_ref_id: number): string | undefined {
-    return this.test.get(node_type)?.get(node_ref_id);
+    return this.ref_nodes.get(node_type)?.get(node_ref_id);
   }
 
   addNode(label: string, node_type: NodeShape, node_color: Color, data?: any): void {
-    let n: NodeGraph = { label: label, shape: node_type, title: `${node_type} Test`, data: data , color: node_color}
-    let id = this.dataSet_nodes.add(n)[0] as unknown as string;
+    let node: NodeGraph = { label: label, shape: node_type, title: `${node_type} ${data.id}`, data: data, color: node_color }
+    let id = this.dataSet_nodes.add(node)[0] as unknown as string;
     console.log("ID node geenered ", id);
-
-    this._registerNewNode(node_type, data.id, id);
+    if (!this._registerNewNode(node_type, data.id, id)) this.dataSet_nodes.remove(node);
   }
 
   addEdge(from: { type: NodeShape, id: number }, to: { type: NodeShape, id: number }): void {
-
-    // Reccupere les references des identifiants dans le graphe et si elles n'existent pas, arrête le processus
     const origin_node = this._getNodeRef(from.type, from.id);
     const arrive_node = this._getNodeRef(to.type, to.id);
     if (!origin_node || !arrive_node) return;
 
-    let is_edge_unique = true;
-
-    this._network.getConnectedEdges(origin_node).forEach(id => {
-      let e = this.dataSet_edges.get(id);
-      if (e && e.to == arrive_node) is_edge_unique = false;
+    this.dataSet_edges.forEach(edges => {
+      if (edges.from === origin_node && edges.to === arrive_node) return
+      if (edges.from === arrive_node && edges.to === origin_node) return
     })
 
-    if (is_edge_unique) this.dataSet_edges.add({ from: origin_node, to: arrive_node });
+    this.dataSet_edges.add({ from: origin_node, to: arrive_node });
   }
 
-  removeNode(node: NodeGraph): void {
-    if (node.id) this.dataSet_nodes.remove(node.id);
+  removeNode(node_type: NodeShape, node_id: number): void {
+    const n = this._getNodeRef(node_type, node_id);
+    if (n) this.dataSet_nodes.remove(n);
   }
+
   removeEdge(edge: EdgeGraph): void {
     if (edge.id) this.dataSet_edges.remove(edge.id);
   }
@@ -121,25 +123,22 @@ export class ForgeGraph {
     let node = this.dataSet_nodes.get(id);
     if (node) node.data = data;
   }
+
   editEdge(id: number, data: any): void {
     let edge = this.dataSet_edges.get(id);
     if (edge) edge.data = data;
   }
 
   clear(): void {
-    this.dataSet_edges.clear()
-    this.dataSet_nodes.clear()
+    this.dataSet_edges.clear();
+    this.dataSet_nodes.clear();
+    this.ref_nodes.clear();
+    this.reverse_ref_node.clear();
   }
 
-  getNodes(): NodeGraph[] {
-    let rep: NodeGraph[] = [];
-    this.dataSet_nodes.distinct("id").forEach(r => {
-      let node = this.getNodeByID(r as number);
-      if (node) rep.push(node)
-    });
-    return rep;
-  }
-  getEdegsIDs() {
-    throw new Error('WIP Method not implemented.');
+  getSelectedNodes(): { type: NodeShape, id: number }[] {
+    return this._network.getSelectedNodes()
+      .map(e => this._getReverseNodeRef(e as unknown as string))
+      .filter((el): el is { type: NodeShape, id: number } => el !== undefined);
   }
 }
--- src/app/test-graph/test-graph.ts
+++ src/app/test-graph/test-graph.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
-import { ForgeGraph, GroupApiService, NodeShape, ProjectApiService } from 'ngx-forge-map';
+import { ForgeGraph, GroupApiService, NodeShape, ProjectApiService, UserApiService } from 'ngx-forge-map';
 
 
 @Component({
@@ -18,6 +18,7 @@ export class TestGraph implements OnInit {
   constructor(
     private readonly projectsAPI: ProjectApiService,
     private readonly groupAPI: GroupApiService,
+    private readonly userAPI: UserApiService,
   ) { }
 
   ngOnInit() {
@@ -27,10 +28,9 @@ export class TestGraph implements OnInit {
   onSearch(_t3: HTMLInputElement) {
     this.projectsAPI.searchProjects(_t3.value).subscribe(projects => {
       this.network.clear()
-      projects.forEach(p => {
-        this.network.addNode(p.name, NodeShape.SQUARE, { background: "#E0AC54" }, p)
+      projects.forEach(project => {
+        this.createProject(project)
       })
-      this.onShowUser();
     })
   }
 
@@ -38,17 +38,65 @@ export class TestGraph implements OnInit {
     this.network.getNodes().forEach(n => {
       this.projectsAPI.getProjectUsers(n.data!.id! as number).subscribe(users => {
         users.forEach(user => {
-          this.network.addNode(user.name, NodeShape.DOT, { background: "#78B1DD" }, user)
+          this.createUser(user)
           this.network.addEdge({ id: n.data.id, type: NodeShape.SQUARE }, { id: user.id, type: NodeShape.DOT })
         })
       })
       this.projectsAPI.getProjectGroups(n.data.id! as number).subscribe(groups => {
         groups.forEach(group => {
-          this.network.addNode(group.name, NodeShape.TRIANGLE, { background: "#55D764" }, group);
+          this.createGroup(group);
           this.network.addEdge({ id: n.data.id, type: NodeShape.SQUARE }, { id: group.id, type: NodeShape.TRIANGLE })
         })
       })
     });
   }
 
+  onExpend() {
+    this.network.getSelectedElements().forEach(e => {
+      switch (e.type) {
+        case NodeShape.DOT:
+          this.userAPI.getUserProjects(e.id as unknown as string).subscribe(projects => {
+            projects.forEach(project => {
+              this.createProject(project);
+              this.network.addEdge({ id: project.id, type: NodeShape.SQUARE }, { id: e.id, type: NodeShape.DOT });
+            });
+          });
+          break;
+        case NodeShape.SQUARE:
+          this.projectsAPI.getProjectUsers(e.id).subscribe(users => {
+            users.forEach(user => {
+              this.createUser(user);
+              this.network.addEdge({ id: user.id, type: NodeShape.DOT }, { id: e.id, type: NodeShape.SQUARE });
+            });
+          });
+          this.projectsAPI.getProjectGroups(e.id).subscribe(groups => {
+            groups.forEach(group => {
+              this.createGroup(group);
+              this.network.addEdge({ id: group.id, type: NodeShape.TRIANGLE }, { id: e.id, type: NodeShape.SQUARE });
+            });
+          });
+          break;
+        case NodeShape.TRIANGLE:
+          this.groupAPI.getGroupProjects(e.id).subscribe(projects => {
+            projects.forEach(project => {
+              this.createProject(project);
+              this.network.addEdge({ id: project.id, type: NodeShape.SQUARE }, { id: e.id, type: NodeShape.TRIANGLE });
+            });
+          });
+          break;
+      }
+    })
+  }
+
+  createUser(user: any) {
+    this.network.addNode(user.name, NodeShape.DOT, { background: "#78B1DD" }, user);
+  }
+
+  createProject(project: any) {
+    this.network.addNode(project.name, NodeShape.SQUARE, { background: "#E0AC54" }, project)
+  }
+
+  createGroup(group: any) {
+    this.network.addNode(group.name, NodeShape.TRIANGLE, { background: "#55D764" }, group);
+  }
 }
