a40eb3742ed61504e9597ebd45867dcd49e76fa0
Tangi Beneat <tangi.beneat@gmail.com>
2025-10-23 11:04:50 +0200

    add routing path for basics pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TestGraph } from './test-graph/test-graph';
+import { TopicFinder } from './topic-finder/topic-finder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TestGraph,
+  },
+  {
+    path: 'subjects',
+    component: TopicFinder,
+  },
+];
