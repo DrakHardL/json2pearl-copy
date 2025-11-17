30acbb723dbf84b0f2ed96ae3f04d922558c4d60
Tangi Beneat <tangi.beneat.etu@univ-lemans.fr>
2025-11-17 12:36:07 +0100

    implements save and load beta favoris

--- projects/ngx-forge-map/src/lib/emcapsulation/graph-forge.ts
+++ projects/ngx-forge-map/src/lib/emcapsulation/graph-forge.ts
@@ -188,4 +188,14 @@ export class GraphForge {
   selectNode(id: string) {
     this._network.selectNodes([id]);
   }
+
+  getNodesIDByType(type: number): number[] {
+    let nodes: NodeGraph[] = this.dataSet_nodes.get();
+    console.log(nodes);
+
+    nodes = nodes.filter((n) => (n.id?.toString().split('==')[1] as unknown as number) == type);
+    console.log(nodes);
+
+    return nodes.map((n) => n.id!.toString().split('==')[2] as unknown as number);
+  }
 }
--- src/app/test-project-finder/test-project-finder.component.ts
+++ src/app/test-project-finder/test-project-finder.component.ts
@@ -1,6 +1,12 @@
 import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GraphForge, NodeShape, ProjectApiService } from 'ngx-forge-map';
+import {
+  GraphForge,
+  GroupApiService,
+  NodeShape,
+  ProjectApiService,
+  UserApiService,
+} from 'ngx-forge-map';
 
 enum NodeColor {
   PROJECT = '#E0AC54',
@@ -28,10 +34,19 @@ export class TestProjectFinder implements OnInit {
 
   network!: GraphForge;
 
-  constructor(private readonly projectsAPI: ProjectApiService) {}
+  constructor(
+    private readonly projectsAPI: ProjectApiService,
+    private readonly groupAPI: GroupApiService,
+    private readonly userAPI: UserApiService
+  ) {}
 
   ngOnInit(): void {
     this.network = new GraphForge(this.visNetwork.nativeElement);
+
+    if (history.state.data) {
+      return this.load_state();
+    }
+
     this.route.queryParams.subscribe((params) => {
       this.topic = params['topic'] || '';
 
@@ -49,6 +64,66 @@ export class TestProjectFinder implements OnInit {
     });
   }
 
+  private load_state(): void {
+    let ids_projects: string[] = history.state.data.projects || [];
+    if (ids_projects[0] == '') {
+      ids_projects = [];
+    }
+    let ids_users: string[] = history.state.data.users || [];
+    if (ids_users[0] == '') {
+      ids_users = [];
+    }
+    let ids_groups: string[] = history.state.data.groups || [];
+    if (ids_groups[0] == '') {
+      ids_groups = [];
+    }
+
+    ids_projects.forEach((id) => {
+      this.projectsAPI.getProject(id as unknown as number).subscribe((project) => {
+        const id_1 = this.createNode(
+          project.name,
+          NodeType.PROJECT,
+          NodeShape.SQUARE,
+          NodeColor.PROJECT,
+          project
+        );
+
+        this.projectsAPI.getProjectUsers(id as unknown as number).subscribe((users) => {
+          users.forEach((user) => {
+            if (ids_users.indexOf(user.id.toString()) != -1) {
+              const id_2 = this.createNode(
+                user.name,
+                NodeType.USER,
+                NodeShape.DOT,
+                NodeColor.USER,
+                user
+              );
+              this.connect_2_nodes(id_1, id_2);
+            }
+          });
+        });
+        this.projectsAPI.getProjectGroups(id as unknown as number).subscribe((groups) => {
+          groups.forEach((group) => {
+            if (ids_groups.indexOf(group.id.toString()) != -1) {
+              const id_2 = this.createNode(
+                group.name,
+                NodeType.GROUP,
+                NodeShape.TRIANGLE,
+                NodeColor.GROUP,
+                group
+              );
+              this.connect_2_nodes(id_1, id_2);
+            }
+          });
+        });
+      });
+    });
+  }
+
+  private connect_2_nodes(id_1: string, id_2: string) {
+    return this.network.connectNodes(id_1, id_2);
+  }
+
   private createNode(
     label: string,
     type: number,
@@ -58,5 +133,4 @@ export class TestProjectFinder implements OnInit {
   ): string {
     return this.network.createNode(label, type, shape, color, data);
   }
-
 }
