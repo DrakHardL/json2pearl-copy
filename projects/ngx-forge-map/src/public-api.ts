9b311044eb6febdf5e55c7c9d5a1828088018cd1
Tangi Beneat <tangi.beneat.etu@univ-lemans.fr>
2025-11-21 13:16:10 +0100

    update import in liberary

--- projects/ngx-forge-map/src/public-api.ts
+++ projects/ngx-forge-map/src/public-api.ts
@@ -3,7 +3,7 @@ export * from './lib/services/group.service';
 export * from './lib/services/topic.service';
 export * from './lib/services/user.service';
 
-export * from './lib/emcapsulation/graph-forge';
+export * from './lib/model/graph-forge';
 
 export * from './lib/model/group.model';
 export * from './lib/model/project.model';
